6ddec9dc6e3f3a1fba6338af44f209cf53aa957b
anupulu <132809300+anupulu@users.noreply.github.com>
2024-12-09 10:33:24 +0100

    Fix GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/anupulu.github.io' : '',
 }
 
 module.exports = nextConfig 
