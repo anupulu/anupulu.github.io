32f52fdea58cf8cdaf327d3f852960400d21bb4f
Anu <132809300+anupulu@users.noreply.github.com>
2025-01-09 11:38:26 +0100

    Update layout.tsx - add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 import './globals.css'
 import Navbar from './components/Navbar'
 
@@ -49,6 +50,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" type="image/png" href="/sprout.png" />
+      </Head>
       <body>
         <Navbar />
         {children}
@@ -56,4 +60,5 @@ export default function RootLayout({
     </html>
   )
 }
+
 
